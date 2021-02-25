bb35b35f6738aadf383bbba7eb36dc952e41e9d4
Musinda Kadhuwa <justinemsinda@gmail.com>
2021-02-25 13:19:57 +0300

    PW-9(task): Configure jest circle-ci and codeclimate

---
+++ src/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+window.scrollTo = jest.fn();
+describe('Renders Main component ', () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+  it('Renders <App/> component correctly ', () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/404! Page Not Found/i)).toBeInTheDocument();
+  });
+  it('Renders navbar correctly', () => {
+    render(<App />);
+    expect(document.querySelector('a').getAttribute('href')).toBe('/');
+  });
+});
